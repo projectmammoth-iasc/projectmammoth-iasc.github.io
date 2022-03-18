271f0811bbc8ccf8d693ad8dd047a6ce7b82a316
projectmammoth-iasc <97911425+projectmammoth-iasc@users.noreply.github.com>
2022-03-18 12:59:14 -0400

    Flick options and VLC medialist added

--- Player/basic_videoplayer_V2.py
+++ Player/basic_videoplayer_V2.py
@@ -1,20 +1,50 @@
 import vlc
 import os
 import time
+import flicklib
 
 HOMEFOLDER = "/home/pi/Desktop/projectmammoth-iasc.github.io-main/Player/Model/"
 PLAYLIST = [file for file in os.walk(HOMEFOLDER)]
 print(PLAYLIST[0][2])
 
 def main():
-    def play_video(video):
-        player = vlc.MediaPlayer(video)
+
+    player = vlc.MediaListPlayer()
+#    vlc.libvlc_toggle_fullscreen()
+
+    def play_video():
+        player.set_media_list(mediaList)
         player.play()
-        player.toggle_fullscreen()
+#         player.toggle_fullscreen()
         
+    playerInstance = vlc.Instance()
+    mediaList = playerInstance.media_list_new()
+    
     for video in PLAYLIST[0][2]:
-        play_video(HOMEFOLDER + video)
+        print(video)
+        media = playerInstance.media_new(HOMEFOLDER+video)
+        mediaList.add_media(media)
         
-       
+    print(playerInstance)
+    
+    play_video()
+        
+#    media = PLAYLIST[0][2][1]
+#    print(media)
+#    play_video(HOMEFOLDER + media)
+
+    @flicklib.flick()
+    def flick(start,finish):
+        global flicktxt
+        flicktxt = start + ' - ' + finish
+        if finish == "west":
+            print(flicktxt)
+            player.previous()
+        elif finish == "east":
+            print(flicktxt)
+            player.next()
+            
+            
+
 if __name__=="__main__":
     main()
